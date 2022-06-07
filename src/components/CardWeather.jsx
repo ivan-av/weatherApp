f7f063bae9272a96eb4691313d17a6ca09798ff6
Iván Avila <ivan.avicamp@gmail.com>
2022-06-07 01:32:42 -0500

    Fix conversor C to F

--- src/components/CardWeather.jsx
+++ src/components/CardWeather.jsx
@@ -6,6 +6,8 @@ const CardWeather = ({latLon, weather}) => {
   
   let [tempType, setTempType] = useState('°C')
   let [tempInfo, setTempInfo] = useState('°F')
+  let [changeTemp, setChangeTemp] = useState(1)
+  let [constFarenheit, setConstFarenheit] = useState(0)
   
 
   const toggleTemp = () => {
@@ -13,10 +15,14 @@ const CardWeather = ({latLon, weather}) => {
     if(tempType ==='°C'){
       tempType = setTempType('°F')
       tempInfo = setTempInfo('°C')
+      changeTemp = setChangeTemp(1.8)
+      constFarenheit = setConstFarenheit(32)
     }
     else {
       tempType = setTempType('°C')
       tempInfo = setTempInfo('°F')
+      changeTemp = setChangeTemp(1)
+      constFarenheit = setConstFarenheit(0)
     }
   }
 
@@ -39,9 +45,9 @@ const CardWeather = ({latLon, weather}) => {
         <div className='card__weather-info'>
           <div className='weather__title'>{weather?.weather[0].main}, {weather?.weather[0].description}</div>
           <div>Clouds: <b>{weather?.clouds.all}%</b></div>
-          <div>Temperature: <b>{(weather?.main.temp).toFixed(1)} {tempType}</b></div>
-          <div>Feels like: <b>{(weather?.main.feels_like).toFixed(1)} {tempType}</b></div>
-          <div>Min - Max: <b>{(weather?.main.temp_min).toFixed(1)} {tempType} - {(weather?.main.temp_max).toFixed(1)} {tempType}</b></div>
+          <div>Temperature: <b>{(weather?.main.temp * changeTemp + constFarenheit).toFixed(1)} {tempType}</b></div>
+          <div>Feels like: <b>{(weather?.main.feels_like * changeTemp + constFarenheit).toFixed(1)} {tempType}</b></div>
+          <div>Min - Max: <b>{(weather?.main.temp_min * changeTemp + constFarenheit).toFixed(1)} {tempType} - {(weather?.main.temp_max * changeTemp + constFarenheit).toFixed(1)} {tempType}</b></div>
           {/* Pressure */}
           <div>Pressure: <b>{weather?.main.pressure} hPa</b></div>
           {/* Humidity */}
