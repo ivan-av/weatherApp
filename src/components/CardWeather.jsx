422a7c2f706ffaa409cfa43f513383b170733c4c
Iván Avila <ivan.avicamp@gmail.com>
2022-06-06 21:10:59 -0500

    Adding styles

--- src/components/CardWeather.jsx
+++ src/components/CardWeather.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
 
+// const [toggleTemp, settoggleTemp] = useState('cent')
+
 const CardWeather = ({latLon, weather}) => {
   return (
     <div className='card-wheater__container'>
+      
       {/* Country and City */}
-      <h1>{weather?.sys.country}, {weather?.name}</h1>
-      {/* Temperature */}
-      <h2>Temperature: {weather?.main.temp.toFixed(1)} °C</h2>
-      <h2>Feels like Temperature: {weather?.main.feels_like.toFixed(1)} °C</h2>
-      <h2>Min. Temperature: {weather?.main.temp_min.toFixed(1)} °C</h2>
-      <h2>Max. Temperature: {weather?.main.temp_max.toFixed(1)} °C</h2>
-      {/* Pressure */}
-      <h2>Pressure: {weather?.main.pressure} hPa</h2>
-      {/* Humidity */}
-      <h2>Humidity: {weather?.main.humidity}%</h2>
-      {/* Wind */}
-      <h2>Wind speed: {weather?.wind.speed.toFixed(1)} meter/sec</h2>
+      <div className='card__country'>
+        {weather?.sys.country}, {weather?.name}
+        </div>
+      
       {/* Weather */}
-      <img src= {`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt="weather icon" />
-      <h2>Weather: {weather?.weather[0].main}, {weather?.weather[0].description}</h2>
-      <h2>Clouds: {weather?.clouds.all}%</h2>
+      <div className='card__weather-section'>
+
+        <div className='card__picture'>  
+          <img className='picture__wheater' src= {`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt="weather icon" />
+        </div>
+
+        <div className='card__weather-info'>
+          <div className='weather__title'>{weather?.weather[0].main}, {weather?.weather[0].description}</div>
+          <div>Clouds: <b>{weather?.clouds.all}%</b></div>
+          <div>Temperature: <b>{weather?.main.temp.toFixed(1)} °C</b></div>
+          <div>Feels like: <b>{weather?.main.feels_like.toFixed(1)} °C</b></div>
+          <div>Min - Max: <b>{weather?.main.temp_min.toFixed(1)} °C - {weather?.main.temp_max.toFixed(1)} °C</b></div>
+          {/* Pressure */}
+          <div>Pressure: <b>{weather?.main.pressure} hPa</b></div>
+          {/* Humidity */}
+          <div>Humidity: <b>{weather?.main.humidity}%</b></div>
+          {/* Wind */}
+          <div>Wind speed: <b>{weather?.wind.speed.toFixed(1)} m/s</b></div>
+        </div>
+
+      </div>
+    
+      <div className='card__button-section'>
+        <button className='card__button'>
+          Change to 
+        </button>
+      </div>
+
     </div>
   )
 }
